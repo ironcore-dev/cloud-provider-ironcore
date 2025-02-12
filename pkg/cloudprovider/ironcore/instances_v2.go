ad6f5131b6f00ffc6a054b17cbff08d24224a654
Damyan Yordanov <damyan.yordanov@sap.com>
2025-02-12 12:37:46 +0100

    Fix IP address handling after rebase

--- pkg/cloudprovider/ironcore/instances_v2.go
+++ pkg/cloudprovider/ironcore/instances_v2.go
@@ -126,17 +126,6 @@ func (o *ironcoreInstancesV2) InstanceMetadata(ctx context.Context, node *corev1
 			})
 		}
 		for _, ip := range networkInterface.Status.IPs {
-			addresses = append(addresses, corev1.NodeAddress{
-				Type:    corev1.NodeInternalIP,
-				Address: ip.String(),
-			})
-		}
-		// TODO remove once the ip handling above has been fixed in ironcore-controller-manager
-		nic := &networkingv1alpha1.NetworkInterface{}
-		if err := o.ironcoreClient.Get(ctx, client.ObjectKey{Namespace: o.ironcoreNamespace, Name: iface.NetworkInterfaceRef.Name}, nic); err != nil {
-			return nil, fmt.Errorf("failed to get network interface %s for machine %s: %w", client.ObjectKeyFromObject(nic), machine.Name, err)
-		}
-		for _, ip := range nic.Status.IPs {
 			if ip.Is6() {
 				addresses = append(addresses, corev1.NodeAddress{
 					Type:    corev1.NodeExternalIP,
